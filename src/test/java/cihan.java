48a32c41982f66c9c7b2e871cad21479cd8c3bde
cihandeligoz <cihan.deligoez@gmail.com>
2021-12-30 20:37:04 +0100

    Cydeo

--- src/test/java/cihan.java
+++ src/test/java/cihan.java
@@ -2,5 +2,6 @@ public class cihan {
     public static void main(String[] args) {
         System.out.println("cd");
         System.out.println("Berlin");
+        System.out.println("Cydeo");
     }
 }
