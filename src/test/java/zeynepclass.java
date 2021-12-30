032fcfdc29a28048c11e101364aeccad4b2b62fb
nzeynepgunduz <nzgunduz5@gmail.com>
2021-12-30 20:37:23 +0100

    Additional commit

--- src/test/java/zeynepclass.java
+++ src/test/java/zeynepclass.java
@@ -2,6 +2,7 @@ public class zeynepclass {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Welcome to my project");
+        System.out.println("Started");
 
 
     }
