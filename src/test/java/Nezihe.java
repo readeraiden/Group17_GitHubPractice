e94ad01d2453cffa32e1811795af95aafbb5fb63
NeZ-B <nezihebingol@gmail.com>
2021-12-30 20:37:28 +0100

    second addition

--- src/test/java/Nezihe.java
+++ src/test/java/Nezihe.java
@@ -2,6 +2,7 @@ public class Nezihe {
     public static void main(String[] args) {
 
         System.out.println("hello friends");
+        System.out.println("thanks Max, Now I understand Github :)");
 
     }
 }
