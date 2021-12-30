2de7b0bdaac75ba04202d1415482c107011d5f0e
antiochian09 <f.pat90@gmail.com>
2021-12-30 20:36:04 +0100

    more work from fpat

--- src/test/java/f_pat_class.java
+++ src/test/java/f_pat_class.java
@@ -1,5 +1,6 @@
 public class f_pat_class {
     public static void main(String[] args) {
         System.out.println("Fpat works");
+        System.out.println("Some more work from fpat");
     }
 }
