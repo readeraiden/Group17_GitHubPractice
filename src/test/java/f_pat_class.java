90535abfd4f7b4cf26dc76114831daf25a1d2eff
antiochian09 <f.pat90@gmail.com>
2021-12-30 20:37:20 +0100

    another piece of code

--- src/test/java/f_pat_class.java
+++ src/test/java/f_pat_class.java
@@ -2,5 +2,6 @@ public class f_pat_class {
     public static void main(String[] args) {
         System.out.println("Fpat works");
         System.out.println("Some more work from fpat");
+        System.out.println("another peice of code");
     }
 }
