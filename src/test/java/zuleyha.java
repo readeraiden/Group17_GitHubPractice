0f55b9ba04347d683aa9d0f490a254f2cb9dde16
zkeklik <zlyhkeklik@gmail.com>
2021-12-31 22:36:21 +0100

    added some code

---
+++ src/test/java/zuleyha.java
@@ -0,0 +1,8 @@
+public class zuleyha {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("I am on intellij");
+        System.out.println("i try to understand how to integrate github with intellij");
+    }
+}
