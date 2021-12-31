6c7729b7c45ed70ac3159784913dcd94f60319d5
OnurEU <onur.eu7@gmail.com>
2021-12-31 17:14:42 +0100

    initial commit

---
+++ src/test/java/Onur.java
@@ -0,0 +1,7 @@
+public class Onur {
+    public static void main(String[] args) {
+        System.out.println("Hello my friends");
+        System.out.println("Good luck with the new project");
+        System.out.println("Success to everyone");
+    }
+}
