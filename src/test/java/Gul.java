1157b6b88a3be1d5c594545ec3b5c18719239997
Eric <eric@cybertekschool.com>
2021-12-31 20:39:58 +0100

    Initial Commit

--- src/test/java/Gul.java
+++ src/test/java/Gul.java
@@ -3,6 +3,6 @@ public class Gul {
     public static void main(String[] args) {
 
         System.out.println("Hey Guys");
-        System.out.println("Lets have fun");
+        System.out.println("Lets have fun!");
     }
 }
