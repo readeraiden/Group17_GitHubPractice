e8eb906e62ae45db494ba4d4aeafaf0167c3ace8
BETUL-BUYUKGOZE <betulcakmak23@hotmail.com>
2022-01-01 15:15:57 +0100

    happy new year message

--- src/test/java/betulClass.java
+++ src/test/java/betulClass.java
@@ -1,5 +1,23 @@
 public class betulClass {
     public static void main(String[] args) {
+        System.out.println("Hello Friends!");
 
+        int year= 2022;
+        switch (year){
+            case 2021:
+                System.out.println("Good Bye 2021!");
+                break;
+            case 2022:
+                System.out.println("Happy new year to all");
+                break;
+            default:
+                if(year<2021){
+                    System.out.println("Old years");
+                    break;
+                }else{
+                    System.out.println("Future years");
+                    break;
+                }
+        }
     }
 }
